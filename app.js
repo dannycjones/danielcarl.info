1549eeaf9b42a2f2a0fb690e54759e494b807563
Daniel Carl Jones <dcjones1@sheffield.ac.uk>
2017-04-01 16:42:53 +0100

    Add bootstrap, setup sass, etc., develop very basic home page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var projects = require('./routes/projects');
 
 var app = express();
 
@@ -27,9 +27,10 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
